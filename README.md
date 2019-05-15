01ff9f98be2463618ebbfa4d6968213e44dff3c2
Alexis <alexis.morin.mail@gmail.com>
2019-05-15 16:06:05 -0400

    unity testbed

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Screenwriter
 ## UE4 and Unity plugin to parse screenplays into sequences
+
+
+## How to write 
+
+The plugin parses 
+
+When in doubt, you can always refer to the excellent [screenwriting.info](http://www.screenwriting.info/).
---
+++ Test Projects/Screenwriter-Unity/Packages/com.alexismorin.screenwriter/Editor/screenwriter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class screenwriter : MonoBehaviour {
+
+    static List<string> parsedScrenplay = new List<string> ();
+
+    static string currentLocation;
+    static string currentTimeOfDay;
+    static string currentCharacter;
+
+    [MenuItem ("Tools/Parse Screenplay")]
+    private static void ParseScreenplay () {
+
+        parsedScrenplay = new List<string> ();
+
+        var selectedFilepath = EditorUtility.OpenFilePanel ("Select Screenplay", "", "txt");
+        FileInfo fileInfo = new FileInfo (selectedFilepath);
+        string filePath = fileInfo.FullName;
+        string line = null;
+        StreamReader reader = new StreamReader (filePath);
+
+        using (reader) {
+            do {
+                line = reader.ReadLine ();
+                parsedScrenplay.Add (line);
+                print (line);
+                // m_codeContents += line + "\n";
+            }
+            while (line != null);
+            reader.Close ();
+        }
+    }
+
+    private static void ParseLine () {
+
+    }
+}
